8a3144c66080e90ce3670678a7b0e43449d39e46
sherchel <997078856@qq.com>
2018-10-14 00:44:54 +0800

    V1.1

--- User/AP_INIT.h
+++ User/AP_INIT.h
@@ -5,10 +5,8 @@
 
 #include "bsp_usart1.h"
 #include "bsp_SysTick.h"
-#include "bsp_esp8266.h"
 #include "bsp_led.h"
 #include "bsp_GeneralTim.h"
-#include "AP_test.h"
 
 
 #endif /* __INIT_H */
--- User/main.c
+++ User/main.c
@@ -21,15 +21,13 @@ int main ( void )
 {
 	/* 初始化 */
 		USARTx_Config ();                                                              //初始化串口1
-		ESP8266_Init ();                                                               //初始化WiFi模块使用的接口和外设
 		LED_Init ();
 		GENERAL_TIM_Init();
 
 	  printf("READY\r\n");
-		ESP8266_StaTcpClient_UnvarnishTest ();
 	
 	
-  while ( 1 );
+		while ( 1 );
 	
 	
 }
